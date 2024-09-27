73e01332bd9d736b5abd0ddecb547a33aab8ffc7
sdybybhon <artem563732@mail.ru>
2024-09-27 15:36:51 +0500

    Task 1

---
+++ algorithms/templates/BubbleSort.cs
@@ -0,0 +1,44 @@
+﻿using AlgoritmLab1.algorithms.templates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmLab1.algorithms
+{
+    internal class BubbleSort : Algorithm
+    {
+        protected override void DoAlg(object data)
+        {
+            int[] array = (int[])data;
+            Sort(array);
+            Console.WriteLine("Отсортированный массив: " + string.Join(", ", array));
+        }
+
+        public void Sort(int[] array)
+        {
+            int n = array.Length;
+            bool swapped;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    { 
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                if (!swapped)
+                    break;
+            }
+        }
+    }
+}
